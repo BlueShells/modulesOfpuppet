967244eaa9a4c855e2ca7f871beb2d7f9c8c5362
zhizhang <zhizhang@redhat.com>
2015-01-26 17:15:30 +0800

    use template

--- vsftpd/manifests/vhost.pp
+++ vsftpd/manifests/vhost.pp
@@ -2,8 +2,7 @@ define vsftpd::vhost($port){
   file{'vhost.conf':
 	path 	=> '/tmp/vhost.conf',
 	ensure 	=> file,
-	content	=> template('vsftpd/vhost.conf.erb')
-
+	content	=> template('vsftpd/vhost.conf.erb'),
   }
-
+      notify {"the port is $port":}
 }
